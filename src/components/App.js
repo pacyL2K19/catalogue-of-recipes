183f9c2b59ec9a71530ab3cab8837ff039dc25ad
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-08-04 00:04:23 +0100

    Minor update on the home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const App = () => (
-  <div className="App">
-    <h2>Catalogue of recipes</h2>
-  </div>
-);
+const App = () => {
+  const [recipes, setRecipes] = useState([]);
+  useEffect(() => {
+    fetch(process.env.REACT_APP_HOST, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((recipes_) => {
+        setRecipes(recipes_);
+        console.log('RECIPES ==>', recipes_);
+      })
+      .catch((err) => {
+        console.log('ERROR ==>', err);
+      });
+  }, []);
+  return (
+    <div className="App">
+      <h2>Catalogue of recipes</h2>
+      <p>
+        {recipes}
+      </p>
+    </div>
+  );
+};
 
 export default App;
