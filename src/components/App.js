0d86d57fcdce2642c7a8e618b82a10dec1786de0
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-08-03 22:43:28 +0100

    Fix linter issues

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <div className="App">
-      <h2>Catalogue of recipes</h2>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h2>Catalogue of recipes</h2>
+  </div>
+);
 
 export default App;
