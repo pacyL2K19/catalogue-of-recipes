d9961e9fa5feb7d5eebfe1130545b6e25fd45db5
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-08-03 22:47:08 +0100

    Update the linter issue

---
+++ src/components/Details.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Details = () => (
+  <>
+    <h2>Details</h2>
+  </>
+);
+
+export default Details;
---
+++ src/containers/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <>
+    <h2>Home</h2>
+  </>
+);
+
+export default Home;
