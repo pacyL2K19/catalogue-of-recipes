eef0bd11250d6374e5944324726df3c5c9096f70
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-08-04 00:35:42 +0100

    Prepare the api

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,24 @@ import React, { useEffect, useState } from 'react';
 const App = () => {
   const [recipes, setRecipes] = useState([]);
   useEffect(() => {
-    fetch(process.env.REACT_APP_HOST, {
+    fetch(`${process.env.REACT_APP_HOST}?q=chicken&app_id=${process.env.REACT_APP_AUTH}&app_key=${process.env.REACT_APP_KEY}`, {
       method: 'GET',
       headers: {
-        'Content-type': 'application/json',
-        Accept: 'application/json',
+        'cache-control': 'private',
+        connection: 'keep-alive',
+        'content-encoding': 'gzip',
+        'content-type': 'application/json',
+        date: 'Tue, 03 Aug 2021 22:41:35 GMT',
+        expires: 'Thu, 01 Jan 1970 00:00:00 GMT',
+        server: 'openresty',
+        'strict-transport-security': 'max-age=15552001',
+        'transfer-encoding': 'chunked',
+        vary: 'accept-encoding',
+        'x-envoy-upstream-service-time': '160',
+        'x-request-id': '9ea9389ac19e27957f9d9c6434578f39',
+        'x-served-by': 'ip-10-0-1-108.ec2.internal/10.0.1.108',
       },
+      mode: 'cors',
     })
       .then((recipes_) => {
         setRecipes(recipes_);
