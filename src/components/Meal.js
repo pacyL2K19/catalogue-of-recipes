b8c705bbf87390ba5d2006e91569dacf859c920e
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-08-05 22:30:35 +0100

    Update the Meal card

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -7,18 +7,18 @@ import styles from '../styles/Meal.module.css';
 const cx = classNames.bind(card);
 
 const Meal = ({ meal }) => (
-  <div className={cx('card')}>
-    <Link to={`/meal/${meal.idMeal}`} className={cx('card-link')}>
-      <img className={cx('card-img')} src={meal.strMealThumb} alt={meal.strMeal} />
-      <div className={cx('card-body')}>
-        <p className={cx('card-subtitle')}>
+  <div className="card">
+    <Link to={`/meal/${meal.idMeal}`} className="card-link">
+      <img className="card-img" src={meal.strMealThumb} alt={meal.strMeal} />
+      <div className="card-body">
+        <p className="card-subtitle">
           <span>Serves</span>
           <span className={cx(styles['bold-info'])}>5</span>
           <span className={cx(styles.divider)}>|</span>
           <span>Prep time</span>
           <span className={cx(styles['bold-info'])}>20 mins</span>
         </p>
-        <h3 className={cx('card-title')}>
+        <h3 className="card-title">
           {meal.strMeal}
         </h3>
       </div>
