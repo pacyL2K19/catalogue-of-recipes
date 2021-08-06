dfa516227e780f89f59c931ade4ca8266f6bc0c9
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-08-06 02:53:09 +0100

    Update the card design

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 const MealCard = (props) => {
   const { meal, onClick } = props;
   return (
-    <div className="meal-card-container col-4">
+    <div className="meal-card-container col-4 my-3">
       <img className="img-meal" src={meal.strMealThumb} alt="Meal" />
       <div className="w-100">
-        <h2>{meal.strMeal}</h2>
+        <h2 className="meal-title">{meal.strMeal}</h2>
         {/** STATIC DATA */}
-        <h3>Time to cook: 10min</h3>
-        <h3>Cal: 140Kcal</h3>
+        <h3 className="meal-description">Time to cook: 10min</h3>
+        <h3 className="meal-description">Cal: 140Kcal</h3>
       </div>
       <button onClick={onClick} type="button" className="text-light btn-open-details">
         <i className="fas fa-eye" />
