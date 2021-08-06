2210db42500a0a2f6e962749ee1da48898779169
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-08-06 02:44:03 +0100

    Design the btn

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const MealCard = (props) => {
   const { meal, onClick } = props;
   return (
-    <div className="meal-card-container">
+    <div className="meal-card-container col-3">
       <img className="img-meal" src={meal.strMealThumb} alt="Meal" />
       <div className="w-100">
         <h2>{meal.strMeal}</h2>
@@ -12,7 +12,7 @@ const MealCard = (props) => {
         <h3>Time to cook: 10min</h3>
         <h3>Cal: 140Kcal</h3>
       </div>
-      <button onClick={onClick} type="button" className="text-light">
+      <button onClick={onClick} type="button" className="text-light btn-open-details">
         <i className="fas fa-eye" />
         See More
       </button>
