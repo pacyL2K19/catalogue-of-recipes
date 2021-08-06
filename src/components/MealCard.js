aefc5f994bf07b0bd865f409fb07ce8ceda9ae47
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-08-06 02:48:30 +0100

    Minor update on the styles

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const MealCard = (props) => {
   const { meal, onClick } = props;
   return (
-    <div className="meal-card-container col-3">
+    <div className="meal-card-container col-4">
       <img className="img-meal" src={meal.strMealThumb} alt="Meal" />
       <div className="w-100">
         <h2>{meal.strMeal}</h2>
