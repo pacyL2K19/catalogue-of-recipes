d8e7e23d0c166a4462bac0165f0c946d8e5e10d3
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-08-08 23:25:34 +0100

    Write unit tests for the redux

--- src/redux/reducers/__tests__/categories.test.js
+++
@@ -1,7 +0,0 @@
-import categories from '../categories';
-
-describe('categories reducer', () => {
-  test('should return initial state', () => {
-    expect(categories(undefined, {})).toEqual([]);
-  });
-});
