8d23336673ea4aa3e3fc5373f06479d60587fe02
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-08-08 23:54:55 +0100

    Prepare the meal details page

--- src/containers/MealDetails.js
+++ src/containers/MealDetails.js
@@ -1,18 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { fetchMeal } from '../API/api';
 
 const MealDetails = () => {
   const { id } = useParams();
+  const [meal, setMeal] = useState([]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    // if (id !== meal.idMeal) {
-    //   dispatch(fetchMealById(id));
-    // }
+    if (id) {
+      fetchMeal(id)
+        .then((data) => {
+          setMeal(data);
+          console.log('DATA ==>', data);
+        })
+        .catch((error) => {
+          console.log('ERROR ==>', error);
+        });
+      // dispatch(fetchMealById(id));
+    }
   }, []);
   return (
     <div>
-      {id}
+      {
+        console.log('DATA ==>', meal)
+      }
     </div>
   );
 };
