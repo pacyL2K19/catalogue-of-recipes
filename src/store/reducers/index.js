d739fac87d7975e4a37038471685f5c675145c6d
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-08-09 00:59:11 +0100

    Remove useless files and imports

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,14 +1,10 @@
 import { combineReducers } from 'redux';
-import categoriesReducer from './categories';
 import mealsReducer from './meals';
 import mealReducer from './meal';
-import categoryReducer from './category';
 
 const rootReducer = combineReducers({
-  categories: categoriesReducer,
   meals: mealsReducer,
   meal: mealReducer,
-  category: categoryReducer,
 });
 
 export default rootReducer;
