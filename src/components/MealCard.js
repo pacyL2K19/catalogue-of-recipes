a9a241c4a87353e458907ce744f92fea3363048a
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-08-08 22:37:07 +0100

    Update the link action

--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const MealCard = (props) => {
   const { meal, onClick } = props;
   return (
     <div className="meal-card-container col-3 my-3">
-      <img className="img-meal" src={meal.strMealThumb} alt="Meal" />
+      <Link to={`/meal/${meal.idMeal}`}><img className="img-meal" src={meal.strMealThumb} alt="Meal" /></Link>
       <div className="w-100 details-bloc">
         <div className="p-3">
           <h2 className="meal-title">{meal.strMeal}</h2>
@@ -13,10 +14,12 @@ const MealCard = (props) => {
           <p className="meal-description mb-1">Time to cook: 10min</p>
           <p className="meal-description mb-1">Cal: 140Kcal</p>
         </div>
-        <button onClick={onClick} type="button" className="text-light p-2 btn-open-details">
-          <i className="fas fa-eye mr-3" />
-          See More
-        </button>
+        <Link to={`/meal/${meal.idMeal}`}>
+          <button onClick={onClick} type="button" className="text-light p-2 btn-open-details">
+            <i className="fas fa-eye mr-3" />
+            See More
+          </button>
+        </Link>
       </div>
     </div>
   );
