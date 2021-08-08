2e60dbf94b8ab792ef772ddb824ca20fe10f128d
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-08-08 23:11:36 +0100

    Add actions and redux config

--- src/containers/MealDetails.js
+++ src/containers/MealDetails.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-const MealDetails = () => (
-  <div />
-);
+const MealDetails = () => {
+  const { id } = useParams();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    // if (id !== meal.idMeal) {
+    //   dispatch(fetchMealById(id));
+    // }
+  }, []);
+  return (
+    <div>
+      {id}
+    </div>
+  );
+};
 
 export default MealDetails;
